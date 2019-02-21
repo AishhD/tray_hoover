cc910383c80e92ef3fd7ddff100910d20af455bf
wimbleclaw <aisha305@hotmail.co.uk>
2019-02-21 22:49:15 +0000

    added input to state

--- src/hoover.js
+++ src/hoover.js
@@ -1,19 +1,10 @@
 const fs = require("fs");
 const parseInputToState = require("./parseInputToState");
 
-//create a state for the data
-const data = () => {
-  state = {
-    roomDimensions: {},
-    hooverPosition: {},
-    dirtPatches: [],
-    drivingInstructions: []
-  };
-};
-
 //synchronously read input
 const readFile = () => {
   let input;
+
   try {
     input = fs.readFileSync("./input.txt", "utf8");
   } catch (err) {
@@ -23,14 +14,23 @@ const readFile = () => {
       throw err;
     }
   }
+
   return input;
 };
 
 const runHoover = () => {
-  //if input file is correctly read run program
+  //create a state for the data
+  state = {
+    roomDimensions: {},
+    hooverPosition: {},
+    dirtPatches: [],
+    drivingInstructions: []
+  };
+
+  //if input file can be correctly read
   if (readFile()) {
-    let input = readFile();
-    parseInputToState(input, data.state);
+    let fileInput = readFile();
+    parseInputToState(fileInput, state);
   }
 };
 
--- src/parseInputToState.js
+++ src/parseInputToState.js
@@ -1,5 +1,40 @@
-const parseInputToState = (input, state) => {
-  console.log(input);
+const formatInput = fileInput => {
+  //split fileInput by line and space
+  let splitLineXY = fileInput.split("\n").map(line => line.split(" "));
+
+  //capitalise directions & split by letter
+  splitLineXY[splitLineXY.length - 1] = splitLineXY[splitLineXY.length - 1][0]
+    .toUpperCase()
+    .split("");
+
+  return splitLineXY;
+};
+
+const parseInputToState = (fileInput, state) => {
+  const formattedInputLines = formatInput(fileInput);
+
+  state.roomDimensions = {
+    x: parseInt(formattedInputLines[0][0]),
+    y: parseInt(formattedInputLines[0][1])
+  };
+
+  state.hooverPosition = {
+    x: parseInt(formattedInputLines[1][0]),
+    y: parseInt(formattedInputLines[1][1])
+  };
+
+  const findDirtPatches = formattedInputLines.slice(2, -1);
+  state.dirtPatches = findDirtPatches.map(position => {
+    return {
+      x: parseInt(position[0]),
+      y: parseInt(position[1])
+    };
+  });
+
+  state.drivingInstructions =
+    formattedInputLines[formattedInputLines.length - 1];
+
+  return state;
 };
 
 module.exports = parseInputToState;
